3f29e78a6ea3bee1252c34c338e08611a1583fba
ZIMMERMANEJ22 <ZimmermanEJ22@gcc.edu>
2025-03-21 00:05:02 -0400

    modified search to work

--- src/main/java/edu/gcc/comp350/Search.java
+++ src/main/java/edu/gcc/comp350/Search.java
@@ -7,12 +7,12 @@ public class Search {
     private ArrayList<String> keywords;
     private ArrayList<Course> searchResults;
 
-//    public Search(ArrayList<String> keywords) {
-//        this.keywords = keywords;
-//    }
-    public Search(ArrayList<Course> example){
-        searchResults = example;
+    public Search(ArrayList<String> keywords) {
+        this.keywords = keywords;
     }
+//    public Search(ArrayList<Course> example){
+//        searchResults = example;
+//    }
     public void SetResults(ArrayList<Course> courses) {
         searchResults = courses;
     }
