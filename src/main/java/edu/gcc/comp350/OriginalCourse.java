eff241b7710d23e7650e602e95efe61431469a2d
josh <126510667+joshkgcc@users.noreply.github.com>
2025-03-20 22:09:26 -0400

    Import Courses Start

---
+++ src/main/java/edu/gcc/comp350/OriginalCourse.java
@@ -0,0 +1,18 @@
+package edu.gcc.comp350;
+
+public class OriginalCourse {
+    int credits;
+    String[] faculty;
+    boolean is_lab;
+    boolean is_open;
+    String location;
+    String name;
+    int number;
+    int open_seats;
+    char section;
+    String semester;
+    String subject;
+    CourseTime[] times;
+    int total_seats;
+}
+
